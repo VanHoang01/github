f875eb62a2f17435c1ac189b88914cb935f20c74
VanHoang01 <you@example.com>
2021-12-06 16:57:56 +0700

    first commit

---
+++ QLDA/DoAnLapTrinhWeb-master/Areas/Admin/Controllers/NhaSanXuatAdminController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Electro.Models;
+using PagedList;
+namespace Electro.Areas.Admin.Controllers
+{
+    public class NhaSanXuatAdminController : Controller
+    {
+        // GET: Admin/NhaSanXuatAdmin
+        ElectroDbContext db = new ElectroDbContext();
+        public ActionResult Index(int? page)
+        {
+            if (Session["Admin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            int iPageNum = (page ?? 1);
+            var lst = db.NhaSanXuats.ToList().OrderBy(n => n.MaNSX).ToPagedList(iPageNum, 10);
+            return View(lst);
+        }
+        [HttpGet]
+        public ActionResult Sua(int MaNSX, string url)
+        {
+            ViewBag.Url = Url;
+            NhaSanXuat nsx = db.NhaSanXuats.SingleOrDefault(n => n.MaNSX == MaNSX);
+            return View(nsx);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Sua(FormCollection f)
+        {
+            int MaNSX = int.Parse(f["MaNSX"]);
+            NhaSanXuat nsx = db.NhaSanXuats.SingleOrDefault(n => n.MaNSX == MaNSX);
+            nsx.TenNSX = f["TenNSX"];
+            nsx.ThongTin = f["ThongTin"].Replace("<p>", "").Replace("</p>", "\n");
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "NhaSanXuatAdmin");
+        }
+        public ActionResult ChiTietDonHang(int MaNSX, string url)
+        {
+            ViewBag.Url = url;
+            var lstChiTietDonHang = db.NhaSanXuats.Select(n => n).Where(n => n.MaNSX == MaNSX);
+            return View(lstChiTietDonHang);
+        }
+        public ActionResult SanPham(int MaNSX, int ? page, string url)
+        {
+            ViewBag.Url = url;
+            ViewBag.MaNSX = MaNSX;
+            int iPageNum = (page ?? 1);
+            int iPageSize = 7;
+            var lst = db.SanPhams.Where(n => n.MaNSX == MaNSX).ToList().OrderBy(n => n.MaSP).ToPagedList(iPageNum, iPageSize);
+            return View(lst);
+        }
+
+        public ActionResult Them(string url)
+        {
+            ViewBag.Url = url;
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Them(FormCollection f)
+        {
+            var nsx = new NhaSanXuat();
+            nsx.TenNSX = f["TenNSX"];
+            nsx.ThongTin = f["ThongTin"].Replace("<p>", "").Replace("</p>", "\n");
+            db.NhaSanXuats.Add(nsx);
+            db.SaveChanges();
+            return RedirectToAction("Index", "NhaSanXuatAdmin");
+        }
+    }
+
+    }
---
+++ QLDA/DoAnLapTrinhWeb-master/Controllers/KhachHangController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Electro.Models;
+
+namespace Electro.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        ElectroDbContext db = new ElectroDbContext();
+        // GET: KhachHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult DangKy(FormCollection collection, KhachHang kh)
+        {
+            var sHoTen = collection["HoTen"];
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["MatKhau"];
+            var sMatKhauNhapLai = collection["MatKhauNL"];
+            var sDiaChi = collection["DiaChi"];
+            var sEmail = collection["Email"];
+            var sDienThoai = collection["DienThoai"];
+
+            if (String.IsNullOrEmpty(sHoTen))
+            {
+                ViewData["err1"] = "Họ tên không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["err2"] = "Tên tài khoản không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["err3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(sMatKhauNhapLai))
+            {
+                ViewData["err4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(sEmail))
+            {
+                ViewData["err5"] = "Email không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sDienThoai))
+            {
+                ViewData["err6"] = "Số điện thoại không được rỗng";
+            }
+            else if (db.KhachHangs.SingleOrDefault(n => n.TaiKhoan == sTenDN) != null)
+            {
+                ViewBag.ThongBao = "Tên đăng nhập đã tồn tại";
+            }
+            else if (db.KhachHangs.SingleOrDefault(n => n.Email == sEmail) != null)
+            {
+                ViewBag.ThongBao = "Email đã được sử dụng";
+            }
+            else
+            {
+                kh.HoTen = sHoTen;
+                kh.TaiKhoan = sTenDN;
+                kh.MatKhau = sMatKhau;
+                kh.Email = sEmail;
+                kh.DiaChi = sDiaChi;
+                kh.DienThoai = sDienThoai;
+                db.KhachHangs.Add(kh);
+                db.SaveChanges();
+                return RedirectToAction("DangNhap");
+            }
+            return this.DangKy();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["MatKhau"];
+            ViewBag.TenDN = "";
+            if (String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["Err1"] = "Bạn chưa nhập tên đăng nhập";
+                return View();
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["Err2"] = "Phải nhập mật khẩu";
+                return View();
+            }
+            else
+            {
+                KhachHang kh = db.KhachHangs.SingleOrDefault(n => n.TaiKhoan == sTenDN && n.MatKhau == sMatKhau);
+                if (kh != null)
+                {
+                    int state = Convert.ToInt32(Request.QueryString["id"]);
+                    ViewBag.ThongBao = "Chúc mừng đăng nhập thành công  ";
+                    ViewBag.GioHang = (from gh in db.GioHangs where gh.MaKH == kh.MaKH select gh).Count();
+                    Session["TaiKhoan"] = kh;
+                    if (state == 1)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return RedirectToAction("DatHang", "GioHang");
+                    }
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không chính xác";
+                    return View();
+                }
+            }
+
+        }
+        public ActionResult DangXuat()
+        {
+            Session["TaiKhoan"] = null;
+            return Redirect("~/Home/Index");
+        }
+
+    }
+}
